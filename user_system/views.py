5a769875dd656b0a34220bfaa9ad14e7442620de
Song Ziming <s.ziming@hotmail.com>
2014-07-23 02:57:09 +0800

    添加了模板引擎
    在项目根目录下，新建templates目录，其中保存的就是模板文件，并在相应App的views.py中进行渲染，生成HTML页面。

--- user_system/views.py
+++ user_system/views.py
@@ -1,10 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import RequestContext, loader
+from django.shortcuts import render
 
 # Create your views here.
 
 def default(request):
-    return HttpResponse("Default view of user_system")
+    # return HttpResponse("Default view of user_system")
+#    template = loader.get_template('default.html')
+    #context = RequestContext(request,
+    context = {
+        'list': [
+            {'text': '123'},
+            {'text': 'abc'},
+        ],
+    }#)
+
+    # return HttpResponse(template.render(context))
+    return render(request, 'default.html', context)
 
 def register(request):
     return HttpResponse("Hello. user_system -> view.py -> register")
